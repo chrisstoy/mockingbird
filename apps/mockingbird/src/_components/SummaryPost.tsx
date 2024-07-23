6af295d8594f9f828006ab45fe50f727d467b90d
Chris Stoy <chrisstoy@gmail.com>
2024-07-22 22:57:42 -0400

    improve SummaryPost
     - add action buttons
     - make image round and improve layout
     - put content into a sub-card

--- apps/mockingbird/src/_components/SummaryPost.tsx
+++ apps/mockingbird/src/_components/SummaryPost.tsx
@@ -1,5 +1,10 @@
 import { Post } from '@/_services/post';
 import { auth } from '@/auth';
+import {
+  ChatBubbleLeftEllipsisIcon,
+  HandThumbDownIcon,
+  HandThumbUpIcon,
+} from '@heroicons/react/20/solid';
 import Image from 'next/image';
 
 type Props = {
@@ -16,20 +21,41 @@ export async function SummaryPost({ post }: Props) {
     <div className="card bg-base-100 w-96 shadow-xl">
       <div className="card-body">
         <div className="flex flex-row">
-          <Image
-            src={imageSrc}
-            alt="Profile Picture"
-            width={50}
-            height={50}
-          ></Image>
-          <div className="flex flex-col ml-2">
+          <div className="avatar">
+            <div className="rounded-full">
+              <Image
+                src={imageSrc}
+                alt="Profile Picture"
+                width={42}
+                height={42}
+              ></Image>
+            </div>
+          </div>
+          <div className="flex flex-col ml-2 justify-center">
             <div className="mb-1">{userName}</div>
             <div className="text-xs">
               Posted on {post.createdAt.toLocaleString()}
             </div>
           </div>
         </div>
-        <p>{post.content}</p>
+        <div className="card bg-base-100 shadow-md">
+          <div className="card-body">{post.content}</div>
+        </div>
+        <div className="card-actions flex flex-row">
+          <button className="btn btn-xs">
+            <HandThumbUpIcon className="h-4 w-4" />
+            Like
+          </button>
+          <button className="btn btn-xs">
+            <HandThumbDownIcon className="h-4 w-4" />
+            Dislike
+          </button>
+          <div className="flex-auto"></div>
+          <button className="btn btn-xs">
+            <ChatBubbleLeftEllipsisIcon className="h-4 w-4" />
+            Comment
+          </button>
+        </div>
       </div>
     </div>
   );
