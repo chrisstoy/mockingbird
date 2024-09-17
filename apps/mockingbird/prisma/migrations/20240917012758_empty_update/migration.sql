c66e6940fa7f7c63682d93759cc24007186be5d8
Chris Stoy <chrisstoy@gmail.com>
2024-09-16 23:17:18 -0400

    reset to a baseline magration

--- apps/mockingbird/prisma/migrations/20240917012758_empty_update/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Friends" DROP CONSTRAINT "Friends_userId_fkey";
-
--- DropIndex
-DROP INDEX "Friends_userId_key";
-
--- AddForeignKey
-ALTER TABLE "Friends" ADD CONSTRAINT "Friends_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
