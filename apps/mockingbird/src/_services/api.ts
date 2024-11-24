34f0fcc28fa7fa38f8f47463c331fa21f5353464
Chris Stoy <chrisstoy@gmail.com>
2024-11-24 16:56:18 -0500

    try to use VERCEL_URL

--- apps/mockingbird/src/_services/api.ts
+++ apps/mockingbird/src/_services/api.ts
@@ -3,8 +3,12 @@
 import { env } from '@/../env.mjs';
 
 export async function apiUrlFor(path: string) {
-  const API_HOST = env.VERCEL_URL ?? env.API_HOST;
-  const API_PATH = env.API_PATH; // '/api';
+  const API_HOST = env.VERCEL_URL ?? process.env.VERCEL_URL ?? env.API_HOST; // ex: 'localhost:3000';
+  const API_PATH = env.API_PATH; // ex: '/api';
 
-  return `${API_HOST}${API_PATH}${path}`;
+  console.log(`env.VERCEL_URL: ${env.VERCEL_URL}`);
+  console.log(`process.env.VERCEL_URL: ${process.env.VERCEL_URL}`);
+  console.log(`API_HOST: ${API_HOST}`);
+
+  return `https://${API_HOST}${API_PATH}${path}`;
 }
