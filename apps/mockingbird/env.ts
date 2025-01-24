23af40fc2ace6f616e82318a9edfcd71398be2f4
Chris Stoy <chrisstoy@gmail.com>
2025-01-24 16:13:44 -0500

    remove some comments

--- apps/mockingbird/env.ts
+++ apps/mockingbird/env.ts
@@ -11,11 +11,6 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']).optional(),
 
-    // VERCEL: z.string().optional(),
-    // VECEL_ENV: z.enum(['development', 'preview', 'production']).optional(),
-    // VERCEL_URL: z.string().optional(),
-    // VERCEL_PROJECT_PRODUCTION_URL: z.string().optional(),
-
     LOG_LEVEL: z
       .enum(['error', 'warn', 'info', 'debug', 'trace'])
       .default('info'),
